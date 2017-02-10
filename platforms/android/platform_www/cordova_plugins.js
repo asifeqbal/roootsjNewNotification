553a1b8aa881c64f32048dad8361f44d8d242b9a
asifeqbal <asif.eqbal@vrisini.com>
2017-02-10 19:49:22 +0530

    phonegap-plugin-push add with sender id

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
         "clobbers": [
             "FirebasePlugin"
         ]
+    },
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.1",
-    "cordova-plugin-firebase": "0.1.19"
+    "cordova-plugin-firebase": "0.1.19",
+    "phonegap-plugin-push": "1.9.2"
 };
 // BOTTOM OF METADATA
 });
